9355d3a3b8865411e38fe0bb8726e9de0373ccb5
Jean-Sébastien Hedde <jshedde@scaleway.com>
2021-05-11 16:16:50 +0200

    completions/make: Avoiding Rakefile
    Thanks to @akinomyoga

--- completions/makefile.completion.sh
+++ completions/makefile.completion.sh
@@ -1,3 +1,3 @@
 # Add completion for Makefile
 # see http://stackoverflow.com/a/38415982/1472048
-complete -W "\`find . -maxdepth 1 -name '*akefile' -exec grep -oE '^[a-zA-Z0-9_-]+:([^=]|$)' {} \; | sed 's/[^a-zA-Z0-9_-]*$//'\`" make
+complete -W "\`find . -maxdepth 1 -name '*makefile' -exec grep -oE '^[a-zA-Z0-9_-]+:([^=]|$)' {} \; | sed 's/[^a-zA-Z0-9_-]*$//'\`" make
